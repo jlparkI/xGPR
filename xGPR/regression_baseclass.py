Fixed bugs (incorrect sign) in map gradient calc. Initial tests using LBFGS for map gradient calc were successful. Next, update SGD for map gradient calc and conduct initial testing.

xGPR/regression_baseclass.py
@@ -844,7 +844,8 @@ class GPRegressionBaseclass(ABC):
         """
         bounds = self._run_pretuning_prep(dataset, random_seed, bounds, "exact")
         best_x, best_score, net_iterations = None, np.inf, 0
-        bounds_tuples = list(map(tuple, bounds))
+        bounds_tuples = list(map(tuple, bounds)) + [(None,None) for i in
+                range(self.training_rffs)]
 
         init_hyperparams = self.kernel.get_hyperparams(logspace=True)
         init_params = np.zeros((init_hyperparams.shape[0] + self.training_rffs))
@@ -860,10 +861,10 @@ class GPRegressionBaseclass(ABC):
             res = minimize(cost_fun, options={"maxiter":max_iter},
                         x0 = init_params, args = args,
                         jac = True, bounds = bounds_tuples)
-
             cost = self.approximate_nmll(res.x[:init_hyperparams.shape[0]],
                         dataset, nmll_rank, nmll_probes, random_seed,
                         nmll_iter, nmll_tol)
+            print(res.x)
             net_iterations += res.nfev
             if cost < best_score:
                 best_x = res.x[:init_hyperparams.shape[0]]
@@ -872,8 +873,8 @@ class GPRegressionBaseclass(ABC):
                 print(f"Restart {iteration} completed. Best score is {best_score}.")
 
             init_params[:] = 0
-            init_hyperparams = [rng.uniform(low = bounds[j,0], high = bounds[j,1])
-                    for j in range(bounds.shape[0])]
+            init_hyperparams = np.array([rng.uniform(low = bounds[j,0], high = bounds[j,1])
+                    for j in range(bounds.shape[0])])
             init_params[:init_hyperparams.shape[0]] = init_hyperparams
 
         if best_x is None:

xGPR/scoring_tools/gradient_tools.py
@@ -117,15 +117,15 @@ def map_gradient(hparams, z_data, y_data, dz_dsigma,
     """
     weight_prod = z_data @ weights
     loss = y_data - weight_prod
-    gradient[0] += 0.5 * (loss**2).sum() / hparams[0]**3
-    gradient[1] += (loss * weight_prod).sum() / (hparams[1] * hparams[0]**2)
+    gradient[0] -= 0.5 * (loss**2).sum() / hparams[0]**3
+    gradient[1] -= (loss * weight_prod).sum() / (hparams[1] * hparams[0]**2)
 
     for i in range(dz_dsigma.shape[2]):
         weight_prod = dz_dsigma[:,:,i] @ weights
-        gradient[2 + i,:] += (weight_prod * loss).sum() / hparams[0]**2
+        gradient[2 + i] -= (weight_prod * loss).sum() / hparams[0]**2
 
-    gradient += (loss[:,None] * z_data).sum(axis=0) / hparams[0]**2
-    return 0.5 * float((loss**2).sum()) / hparams[0]**2
+    gradient[hparams.shape[0]:] += (loss[:,None] * z_data).sum(axis=0) / hparams[0]**2
+    return 0.5 * float((loss**2).sum() / hparams[0]**2)
 
 
 def complete_map_grad_calc(gradient, hparams, ndatapoints,
@@ -146,19 +146,30 @@ def complete_map_grad_calc(gradient, hparams, ndatapoints,
             values.
         a_reg (float): A regularization value that penalizes hyperparameter
             values which might cause overfitting.
+
+    Returns:
+        loss (float): The component of the loss resulting from the regularization
+            terms.
     """
     log_hparams = np.log(hparams)
-    gradient[0] -= ndatapoints * log_hparams[0]
-    gradient[0] -= (log_hparams[0] + constants.LAMBDA_HPRIOR) / (hparams[0] * a_reg**2)
+    loss = 0.5 * weights.shape[0] * float(weights.T @ weights) / hparams[1]**2
+    loss += weights.shape[0] * log_hparams[1] + ndatapoints * log_hparams[0]
+    loss += 0.5 * log_hparams[1] / (a_reg**2)
+    loss += 0.5 * (log_hparams[0] + constants.LAMBDA_HPRIOR) / (a_reg**2)
 
-    gradient[1] -= weights.shape[0] / hparams[1]
-    gradient[1] -= log_hparams[1] / (hparams[1] * a_reg**2)
-    gradient[1] += float(weights.T @ weights) / hparams[1]**3
+    gradient[0] += ndatapoints * log_hparams[0]
+    gradient[0] += (log_hparams[0] + constants.LAMBDA_HPRIOR) / (hparams[0] * a_reg**2)
+
+    gradient[1] += weights.shape[0] / hparams[1]
+    gradient[1] += log_hparams[1] / (hparams[1] * a_reg**2)
+    gradient[1] -= float(weights.T @ weights) / hparams[1]**3
 
     if hparams.shape[0] > 2:
-        gradient[2:hparams.shape] -= (log_hparams[2:] + constants.SIGMA_HPRIOR) / \
+        gradient[2:hparams.shape[0]] += (log_hparams[2:] + constants.SIGMA_HPRIOR) / \
                 (hparams[2:] * a_reg**2)
+        loss += 0.5 * float((log_hparams[2:] + constants.SIGMA_HPRIOR).sum()) / (a_reg**2)
 
-    gradient[hparams.shape[0]:] -= weights / (hparams[1]**2)
+    gradient[hparams.shape[0]:] += weights / (hparams[1]**2)
 
     gradient[:hparams.shape[0]] *= hparams
+    return float(loss)

xGPR/xGP_Regression.py
@@ -974,10 +974,18 @@ class xGPRegression(GPRegressionBaseclass):
         """
         num_hparams = self.kernel.get_hyperparams().shape[0]
 
+        #Unlike full map grad, input params here are either cupy or numpy
+        #array, depending on device -- switching the weights back and forth
+        #for each minibatch would be expensive, and we aren't using a scipy
+        #routine for SGD so don't need to. Only catch: the kernel hparams are always
+        #numpy array. Therefore, if running on gpu, send hparams to kernel
+        #as numpy array before proceeding.
         if self.device == "gpu":
             grad = cp.zeros(params.shape)
+            self.kernel.set_hyperparams(cp.asnumpy(params[:num_hparams]), logspace=True)
         else:
             grad = np.zeros(params.shape)
+            self.kernel.set_hyperparams(params[:num_hparams], logspace=True)
 
         hparams = params[:num_hparams]
         weights = params[num_hparams:]
@@ -1011,25 +1019,31 @@ class xGPRegression(GPRegressionBaseclass):
                 the weight gradients.
         """
         num_hparams = self.kernel.get_hyperparams().shape[0]
+        self.kernel.set_hyperparams(in_params[:num_hparams], logspace=True)
 
+        #Input here is always numpy array (in order to be compatible with
+        #Scipy's LBFGS). Since this func loops over the whole dataset, time
+        #to convert from / to cupy array once at beginning is negligible
+        #fraction of total expense.
         if self.device == "gpu":
             grad = cp.zeros(in_params.shape)
             params = cp.asarray(in_params)
+            hparams, weights = cp.exp(params[:num_hparams]), params[num_hparams:]
         else:
             grad = np.zeros(in_params.shape)
-            params = in_params
-
-        hparams = params[:num_hparams]
-        weights = params[num_hparams:]
+            params = in_params.copy()
+            hparams, weights = np.exp(params[:num_hparams]), params[num_hparams:]
 
         loss = 0.0
         for x_data, y_data in dataset.get_chunked_data():
             xfeatures, dz_dsigma = self.kernel.kernel_specific_gradient(x_data)
             loss += map_gradient(hparams, xfeatures, y_data,
                         dz_dsigma, weights, grad)
-        complete_map_grad_calc(grad, hparams, dataset.get_ndatapoints(),
+        loss += complete_map_grad_calc(grad, hparams, dataset.get_ndatapoints(),
                 weights, a_reg)
 
         if self.device == "gpu":
             grad = cp.asnumpy(grad)
+        if self.verbose:
+            print("Gradient evaluation complete.")
         return loss, grad