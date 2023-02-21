Transferred all map gradient functions to scoring_tools. Double-checked map gradient calculations, fixed error for lambda hyperparameter.

xGPR/regression_baseclass.py
@@ -25,6 +25,8 @@ from .optimizers.pure_bayes_optimizer import pure_bayes_tuning
 from .optimizers.bayes_grid_optimizer import bayes_grid_tuning
 from .optimizers.lb_optimizer import shared_hparam_search
 from .optimizers.crude_grid_optimizer import crude_grid_tuning
+from .scoring_tools.gradient_tools import full_map_gradient, minibatch_map_gradient
+
 
 class GPRegressionBaseclass(ABC):
     """The base class for xGPR regression classes. Provides shared
@@ -852,7 +854,7 @@ class GPRegressionBaseclass(ABC):
         init_params[:init_hyperparams.shape[0]] = init_hyperparams
 
         rng = np.random.default_rng(random_seed)
-        args, cost_fun = (dataset, a_reg), self.full_map_gradient
+        args, cost_fun = (dataset, self.kernel, a_reg, self.verbose), full_map_gradient
 
         if self.verbose:
             print("Now beginning L-BFGS minimization.")

xGPR/scoring_tools/gradient_tools.py
@@ -80,11 +80,114 @@ def exact_nmll_reg_grad(z_trans_z, z_trans_y, y_trans_y,
 
 
 
+def minibatch_map_gradient(params, x_data, y_data, kernel, a_reg):
+    """Calculates the regularized MAP gradient using a
+    minibatch supplied by caller.
+
+    Args:
+        params (np.ndarray): A numpy array of shape
+            num_rffs + num hyperparams. The first num_hyperparams
+            elements are the hyperparameters, the remaining
+            elements are the weights.
+        x_data (array): A cupy or numpy array containing the
+            minibatch for which the gradient will be calculated.
+        y_data (array): A cupy or numpy array containing the associated
+            target (y) values.
+        kernel: A kernel object that can generate random features.
+        a_reg (float): A regularization parameter.
+
+    Returns:
+        loss (float): The loss value for this minibatch.
+        grad (array): A cupy or numpy array (depending on device)
+            containing the gradient for both hyperparameters and
+            weights.
+    """
+    num_hparams = kernel.get_hyperparams().shape[0]
+
+    #Unlike full map grad, input params here are either cupy or numpy
+    #array, depending on device -- switching the weights back and forth
+    #for each minibatch would be expensive, and we aren't using a scipy
+    #routine for SGD so don't need to. Only catch: the kernel hparams are always
+    #numpy array. Therefore, if running on gpu, send hparams to kernel
+    #as numpy array before proceeding.
+    if kernel.device == "gpu":
+        grad = cp.zeros(params.shape)
+        kernel.set_hyperparams(cp.asnumpy(params[:num_hparams]), logspace=True)
+    else:
+        grad = np.zeros(params.shape)
+        kernel.set_hyperparams(params[:num_hparams], logspace=True)
+
+    hparams = params[:num_hparams]
+    weights = params[num_hparams:]
+
+    xfeatures, dz_dsigma = kernel.kernel_specific_gradient(x_data)
+    loss = map_gradient(hparams, xfeatures, y_data,
+                dz_dsigma, weights, grad)
+    complete_map_grad_calc(grad, hparams, x_data.shape[0], weights, a_reg)
+    return loss, grad
+
+
+
+def full_map_gradient(in_params, dataset, kernel, a_reg, verbose):
+    """Calculates the regularized MAP gradient for
+    the full dataset.
+
+    Args:
+        in_params (np.ndarray): A numpy array of shape
+            num_rffs + num hyperparams. The first num_hyperparams
+            elements are the hyperparameters, the remaining
+            elements are the weights.
+        dataset: A Dataset object for the full training dataset.
+        kernel: A valid kernel object that can generate random
+            features.
+        a_reg (float): A regularization parameter.
+        verbose (bool): Whether to print an update after gradient
+            calculation.
+
+    Returns:
+        loss (float): The loss value calculated across the full
+            dataset.
+        gradient (np.ndarray): An array of shape num_rffs +
+            num_hyperparams. The first num_hyperparams elements
+            are the hyperparameter gradients, the remainder are
+            the weight gradients.
+    """
+    num_hparams = kernel.get_hyperparams().shape[0]
+    kernel.set_hyperparams(in_params[:num_hparams], logspace=True)
+
+    #Input here is always numpy array (in order to be compatible with
+    #Scipy's LBFGS). Since this func loops over the whole dataset, time
+    #to convert from / to cupy array once at beginning is negligible
+    #fraction of total expense.
+    if kernel.device == "gpu":
+        grad = cp.zeros(in_params.shape)
+        params = cp.asarray(in_params)
+        hparams, weights = cp.exp(params[:num_hparams]), params[num_hparams:]
+    else:
+        grad = np.zeros(in_params.shape)
+        params = in_params.copy()
+        hparams, weights = np.exp(params[:num_hparams]), params[num_hparams:]
+
+    loss = 0.0
+    for x_data, y_data in dataset.get_chunked_data():
+        xfeatures, dz_dsigma = kernel.kernel_specific_gradient(x_data)
+        loss += map_gradient(hparams, xfeatures, y_data,
+                        dz_dsigma, weights, grad)
+    loss += complete_map_grad_calc(grad, hparams, dataset.get_ndatapoints(),
+                weights, a_reg)
+
+    if kernel.device == "gpu":
+        grad = cp.asnumpy(grad)
+    if verbose:
+        print("Gradient evaluation complete.")
+    return loss, grad
+
+
 
 def map_gradient(hparams, z_data, y_data, dz_dsigma,
         weights, gradient):
     """Calculates the gradient of the training set loss, given specified
-    regularization parameters, for the input data arrays. The
+    regularization parameters, for a single batch of data. The
     regularization loss is not calculated in this function and
     should be added to the result by caller. Calculations are in place
     so nothing is returned.
@@ -152,22 +255,22 @@ def complete_map_grad_calc(gradient, hparams, ndatapoints,
             terms.
     """
     log_hparams = np.log(hparams)
-    loss = 0.5 * weights.shape[0] * float(weights.T @ weights) / hparams[1]**2
+    loss = 0.5 * float(weights.T @ weights) / hparams[1]**2
     loss += weights.shape[0] * log_hparams[1] + ndatapoints * log_hparams[0]
     loss += 0.5 * log_hparams[1] / (a_reg**2)
-    loss += 0.5 * (log_hparams[0] + constants.LAMBDA_HPRIOR) / (a_reg**2)
+    loss += 0.5 * (log_hparams[0] - constants.LAMBDA_HPRIOR) / (a_reg**2)
 
-    gradient[0] += ndatapoints * log_hparams[0]
-    gradient[0] += (log_hparams[0] + constants.LAMBDA_HPRIOR) / (hparams[0] * a_reg**2)
+    gradient[0] += ndatapoints / hparams[0]
+    gradient[0] += (log_hparams[0] - constants.LAMBDA_HPRIOR) / (hparams[0] * a_reg**2)
 
     gradient[1] += weights.shape[0] / hparams[1]
     gradient[1] += log_hparams[1] / (hparams[1] * a_reg**2)
     gradient[1] -= float(weights.T @ weights) / hparams[1]**3
 
     if hparams.shape[0] > 2:
-        gradient[2:hparams.shape[0]] += (log_hparams[2:] + constants.SIGMA_HPRIOR) / \
+        gradient[2:hparams.shape[0]] += (log_hparams[2:] - constants.SIGMA_HPRIOR) / \
                 (hparams[2:] * a_reg**2)
-        loss += 0.5 * float((log_hparams[2:] + constants.SIGMA_HPRIOR).sum()) / (a_reg**2)
+        loss += 0.5 * float((log_hparams[2:] - constants.SIGMA_HPRIOR).sum()) / (a_reg**2)
 
     gradient[hparams.shape[0]:] += weights / (hparams[1]**2)
 