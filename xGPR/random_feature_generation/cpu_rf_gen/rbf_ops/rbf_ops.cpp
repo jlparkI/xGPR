10f9fb68a727cd6f94449b36aea01172e501a830
jlparkinson1 <jlparkinson1@gmail.com>
2023-06-05 10:13:58 -0700

    Minor modifications to docs.

--- xGPR/random_feature_generation/cpu_rf_gen/rbf_ops/rbf_ops.cpp
+++ xGPR/random_feature_generation/cpu_rf_gen/rbf_ops/rbf_ops.cpp
@@ -1,5 +1,5 @@
 /*!
- * # double_rbf_ops.c
+ * # double_rbf_ops.cpp
  *
  * This module performs all major steps involved in feature generation for
  * RBF-type kernels, which includes RBF, Matern, ARD and MiniARD (and by extension
