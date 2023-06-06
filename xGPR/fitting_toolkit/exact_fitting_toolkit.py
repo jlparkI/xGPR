Fixed bug in uncertainty / variance calculation.

xGPR/fitting_toolkit/exact_fitting_toolkit.py
@@ -55,13 +55,10 @@ def calc_variance_exact(kernel, dataset, kernel_choice, variance_rffs):
         var: A cupy or numpy array of shape (M, M) where M is the
             number of random features.
     """
-    #This is a very naughty hack.
-    #TODO: Add a proper variance calc for linear.
-    if kernel_choice == "Linear":
-        return None
     z_trans_z = calc_var_design_mat(dataset, kernel,
                         variance_rffs)
     lambda_ = kernel.get_lambda()
+    #var = z_trans_z.copy()
     z_trans_z.flat[::z_trans_z.shape[0]+1] += lambda_**2
     if kernel.device == "cpu":
         var = np.linalg.pinv(z_trans_z)

xGPR/xGP_Regression.py
@@ -122,6 +122,7 @@ class xGPRegression(GPRegressionBaseclass):
         """
         xdata = self.pre_prediction_checks(input_x, get_var)
         preds, var = [], []
+
         lambda_ = self.kernel.get_lambda()
 
         for i in range(0, xdata.shape[0], chunk_size):
@@ -134,7 +135,7 @@ class xGPRegression(GPRegressionBaseclass):
                     pred_var = (self.var @ xfeatures.T).T
                 else:
                     pred_var = self.var.batch_matvec(xfeatures.T).T
-                pred_var = lambda_**2 * ((xfeatures * pred_var).sum(axis=1))
+                pred_var = lambda_**2 + lambda_**2 * (xfeatures * pred_var).sum(axis=1)
                 var.append(pred_var)
 
         if self.device == "gpu":
@@ -149,6 +150,9 @@ class xGPRegression(GPRegressionBaseclass):
             mempool.free_all_blocks()
         else:
             var = np.concatenate(var)
+        #There may (rarely) be numerical issues which result in variances
+        #less than zero; if so, set those variances to zero.
+        var[var < 0] = 0
         return preds * self.trainy_std + self.trainy_mean, var * self.trainy_std**2
 
 