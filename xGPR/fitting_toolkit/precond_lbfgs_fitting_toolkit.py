d33416c4f80ae8751779b84bf4aabf010a69000d
jlparkinson1 <jlparkinson1@gmail.com>
2023-09-25 20:47:10 -0700

    Rearranged lbfgs fitting code for upcoming modifications.

--- xGPR/fitting_toolkit/precond_lbfgs_fitting_toolkit.py
+++ xGPR/fitting_toolkit/precond_lbfgs_fitting_toolkit.py
@@ -5,8 +5,8 @@ try:
     import cupy as cp
 except:
     pass
-from scipy.optimize import minimize
 
+from scipy.optimize import minimize
 from ..scoring_toolkit.exact_nmll_calcs import calc_zty
 
 
@@ -31,9 +31,10 @@ class lBFGSModelFit:
         dtype: A convenience reference to either cp.float64 or np.float64,
             depending on device.
         niter (int): The number of function evaluations performed.
+        history_size (int): The number of previous gradient updates to store.
     """
 
-    def __init__(self, dataset, kernel, device, verbose):
+    def __init__(self, dataset, kernel, device, verbose, history_size = 5):
         """Class constructor.
 
         Args:
@@ -44,6 +45,8 @@ class lBFGSModelFit:
         device (str): One of 'cpu', 'gpu'. Indicates where calculations
             will be performed.
         verbose (bool): If True, print regular updates.
+        history_size (int): The number of gradient updates to store. 5 is
+            a good default and should not generally be changed.
         """
         self.dataset = dataset
         self.kernel = kernel
@@ -57,12 +60,17 @@ class lBFGSModelFit:
             self.zero_arr = np.zeros
             self.dtype = np.float64
         self.n_iter = 0
+        self.history_size = history_size
 
-    def fit_model_lbfgs(self, max_iter = 500, tol = 3e-09):
+
+    def fit_model_lbfgs(self, preconditioner = None, max_iter = 500, tol = 3e-09):
         """Finds an optimal set of weights using the information already
         provided to the class constructor.
 
         Args:
+            preconditioner: Either None or a valid preconditioner object.
+                If not None, the preconditioner is used as H0 -- the
+                initial approximation to the Hessian.
             max_iter (int): The maximum number of iterations for L_BFGS.
             tol (float): The threshold for convergence. User not currently
                 allowed to specify since setting a larger / smaller tol
@@ -74,16 +82,38 @@ class lBFGSModelFit:
                 best set of weights found. A 1d array of length self.kernel.get_num_rffs().
         """
         z_trans_y, _ = calc_zty(self.dataset, self.kernel)
-        init_weights = np.zeros((self.kernel.get_num_rffs()))
-        res = minimize(self.cost_fun, options={"maxiter":max_iter, "ftol":tol},
+        # If no preconditioner supplied, run a standard minimization using
+        # scipy's routine.
+        if preconditioner is None:
+            init_weights = np.zeros((self.kernel.get_num_rffs()))
+            res = minimize(self.cost_fun, options={"maxiter":max_iter, "ftol":tol},
                     method = "L-BFGS-B",
                     x0 = init_weights, args = (z_trans_y),
                     jac = True, bounds = None)
 
-        wvec = res.x
-        if self.device == "gpu":
-            wvec = cp.asarray(wvec)
-        return wvec, self.n_iter, []
+            wvec = res.x
+            if self.device == "gpu":
+                wvec = cp.asarray(wvec)
+            return wvec, self.n_iter, []
+        return self.preconditioned_lbfgs(preconditioner, z_trans_y)
+
+
+
+    def preconditioned_lbfgs(self, preconditioner, z_trans_y):
+        """Runs L-BFGS using the preconditioner as an approximation
+        to H0.
+
+        Args:
+            preconditioner: A valid preconditioner object.
+            z_trans_y (ndarray): A cupy or numpy array containing the
+                product Z^T y.
+
+        Returns:
+            wvec (np.ndarray): A numpy array containing the final weights.
+        """
+        stored_grads = []
+
+
 
 
     def cost_fun(self, weights, z_trans_y):
