8d92c8ffcbc408f9b25e97ea804d70440f8c1530
Jonathan Parkinson <jlparkinson1@gmail.com>
2023-10-16 20:47:13 -0700

    Updated for internal release of v0.1.3.1 with exact quadratic kernel.

--- xGPR/__init__.py
+++ xGPR/__init__.py
@@ -1,6 +1,6 @@
 #Version number. Updated if generating a new release.
 #Otherwise, do not change.
-__version__ = "0.1.3.0"
+__version__ = "0.1.3.1"
 
 #Key imports.
 from .xGP_Regression import xGPRegression
