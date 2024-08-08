daa0d016a25985a562e57b0a333d4131f67777c1
Jonathan Parkinson <jlparkinson1@gmail.com>
2024-08-08 13:44:51 -0700

    Added xgp classifier to init imports.

--- xGPR/__init__.py
+++ xGPR/__init__.py
@@ -4,6 +4,7 @@ __version__ = "0.4.5"
 
 #Key imports.
 from .xgp_regression import xGPRegression
+from .xgp_classification import xGPClassifier
 from .data_handling.dataset_builder import build_regression_dataset
 from .data_handling.dataset_builder import build_classification_dataset
 
